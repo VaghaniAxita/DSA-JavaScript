4fccf4c59b1f4641713d37aebad2b61ab6a3bd83
VaghaniAxita <vaghaniaxita23@gmail.com>
2024-08-05 10:06:48 +0530

    exam-1

--- EXAM/Exam-1/Q-2_MergeArrays.js
+++ EXAM/Exam-1/Q-2_MergeArrays.js
@@ -1,29 +1,28 @@
 // Question 2: Merge Two Sorted Arrays
-const mergeArray=(num1,m,num2,n)=>{
-    let i=m-1;
-    let j=n-1;
-    let k=m+n-1;
+const mergeArray = (num1, m, num2, n) => {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
 
-    while (i>=0 &&j>=0){
-        if(num1[i]>num2[j]){
-            num1[k]=num1[i];
+    while (i >= 0 && j >= 0) {
+        if (num1[i] > num2[j]) {
+            num1[k] = num1[i];
             i--;
-        }
-        else{
-            num1[k]=num2[j];
+        } else {
+            num1[k] = num2[j];
             j--;
         }
         k--;
     }
 
-    while (j>=0){
-        num1[k]=num2[j];
+    while (j >= 0) {
+        num1[k] = num2[j];
         j--;
         k--;
     }
 }
 
-let num1=[1,3,5]
-let num2=[2,4,6]
-console.log(mergeArray);
-
+let num1 = [1, 3, 5];
+let num2 = [2, 4, 6];
+mergeArray(num1, 3, num2, 3);
+console.log(num1);
